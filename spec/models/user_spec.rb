618b7cdbeb5e0b0b12dc87112c85e1013ed5f346
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-10-28 06:32:46 +0100

    add unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
+
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_numericality_of(:posts_counter).only_integer.is_greater_than_or_equal_to(0) }
+  describe 'validations' do
+    it 'is valid with a name' do
+      user = User.new(name: 'John Doe', posts_counter: 3)
+      expect(user).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      user = User.new(name: nil)
+      expect(user).to_not be_valid
+    end
+
+    it 'is not valid with a negative posts_counter' do
+      user = User.new(name: 'Alice', posts_counter: -1)
+      expect(user).to_not be_valid
+    end
+  end
+  describe '#recent_posts' do
+    it 'returns the 3 most recent posts' do
+      user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+      post1 = Post.create(author: user, title: 'Post 1', text: 'hello world', comments_counter: 0,
+                          likes_counter: 0, created_at: 2.days.ago)
+      post2 = Post.create(author: user, title: 'Post 2', text: 'hello world', comments_counter: 0,
+                          likes_counter: 0, created_at: 2.days.ago)
+      post3 = Post.create(author: user, title: 'Post 3', text: 'hello world', comments_counter: 0,
+                          likes_counter: 0, created_at: 2.days.ago)
+      rec_posts = user.recent_posts
+      expect(rec_posts).to eq([post3, post2, post1])
+    end
+  end
 end
