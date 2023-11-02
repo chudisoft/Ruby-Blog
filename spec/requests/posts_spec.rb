1753bc3b61f81710739d1bbc1cfcd588676a30c0
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-11-02 14:55:03 +0100

    test controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,90 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /users/:user_id/posts' do
-    let(:user) { create(:user) }
-    let!(:posts) { create_list(:post, 3, author: user) }
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :request do
+  describe 'GET #index' do
+    before { get user_posts_path(user_id: 1) }
 
     it 'returns a successful response' do
-      get user_posts_path(user)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
     end
 
     it 'renders the index template' do
-      get user_posts_path(user)
       expect(response).to render_template(:index)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get user_posts_path(user)
-      expect(response.body).to include('Here is a list of posts by the user')
-    end
-
-    it 'lists all posts by the user' do
-      get user_posts_path(user)
-      posts.each do |post|
-        expect(response.body).to include(post.title)
-      end
+      expect(response.body).to include('Here is a list of posts for given user')
     end
   end
 
-  describe 'GET /users/:user_id/posts/:id' do
-    let(:user) { create(:user) }
-    let(:post) { create(:post, author: user) }
+  describe 'GET #show' do
+    before { get user_post_path(user_id: 1, id: 1) }
 
     it 'returns a successful response' do
-      get user_post_path(user, post)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
     end
 
     it 'renders the show template' do
-      get user_post_path(user, post)
       expect(response).to render_template(:show)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get user_post_path(user, post)
-      expect(response.body).to include("Details of the post '#{post.title}'")
-    end
-  end
-
-  describe 'POST /users/:user_id/posts' do
-    let(:user) { create(:user) }
-    let(:post_params) { { title: 'New Post', text: 'Sample text' } }
-
-    it 'creates a new post for the user' do
-      post user_posts_path(user), params: { post: post_params }
-      expect(response).to have_http_status(302) # Redirect after creation
-      expect(user.posts.last.title).to eq('New Post')
-    end
-  end
-
-  describe 'GET /users/:user_id/posts/:id/edit' do
-    let(:user) { create(:user) }
-    let(:post) { create(:post, author: user) }
-
-    it 'returns a successful response' do
-      get edit_user_post_path(user, post)
-      expect(response).to have_http_status(200)
-    end
-
-    it 'renders the edit template' do
-      get edit_user_post_path(user, post)
-      expect(response).to render_template(:edit)
-    end
-  end
-
-  describe 'PATCH /users/:user_id/posts/:id' do
-    let(:user) { create(:user) }
-    let(:post) { create(:post, author: user) }
-    let(:updated_attributes) { { title: 'Updated Post', text: 'Updated text' } }
-
-    it 'updates the post' do
-      patch user_post_path(user, post), params: { post: updated_attributes }
-      expect(response).to have_http_status(302) # Redirect after update
-      post.reload
-      expect(post.title).to eq('Updated Post')
-      expect(post.text).to eq('Updated text')
+      expect(response.body).to include('Here is the details of post')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,66 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /users' do
-    it 'returns a successful response' do
-      get '/users'
-      expect(response).to have_http_status(200)
+RSpec.describe UsersController, type: :request do
+  describe 'GET #index' do
+    before { get users_path }
+
+    it 'returns a 200 OK status' do
+      expect(response).to have_http_status(:ok)
     end
 
     it 'renders the index template' do
-      get '/users'
       expect(response).to render_template(:index)
     end
 
-    it 'includes correct placeholder text in the response body' do
-      get '/users'
+    it 'includes the correct placeholder text in the response body' do
       expect(response.body).to include('Here is a list of users')
     end
   end
 
-  describe 'GET /users/:id' do
-    let(:user) { create(:user) }
+  describe 'GET #show' do
+    before { get user_path(1) }
 
-    it 'returns a successful response' do
-      get user_path(user)
-      expect(response).to have_http_status(200)
+    it 'returns a 200 OK status' do
+      expect(response).to have_http_status(:ok)
     end
 
     it 'renders the show template' do
-      get user_path(user)
       expect(response).to render_template(:show)
     end
 
-    it 'includes correct placeholder text in the response body' do
-      get user_path(user)
-      expect(response.body).to include("Details of user #{user.name}")
-    end
-  end
-
-  describe 'GET /users/:id/edit' do
-    let(:user) { create(:user) }
-
-    it 'returns a successful response' do
-      get edit_user_path(user)
-      expect(response).to have_http_status(200)
-    end
-
-    it 'renders the edit template' do
-      get edit_user_path(user)
-      expect(response).to render_template(:edit)
-    end
-  end
-
-  describe 'PATCH /users/:id' do
-    let(:user) { create(:user) }
-    let(:updated_attributes) { { name: 'Updated User', bio: 'Updated bio' } }
-
-    it 'updates the user' do
-      patch user_path(user), params: { user: updated_attributes }
-      expect(response).to have_http_status(302) # Redirect after update
-      user.reload
-      expect(user.name).to eq('Updated User')
-      expect(user.bio).to eq('Updated bio')
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('Here are the details of the user')
     end
   end
 end
